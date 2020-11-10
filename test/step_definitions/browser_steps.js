30e0f76b9f75d1b79cd38f14f624ae90d9f77e65
watsonik <watsonik@gmail.com>
2020-11-10 23:36:10 +0300

    Use {string} {int} instead of regExp

--- test/step_definitions/browser_steps.js
+++ test/step_definitions/browser_steps.js
@@ -1,15 +1,15 @@
 const { When, Then, setDefaultTimeout } = require("cucumber");
 const { expect } = require("chai");
 setDefaultTimeout(60000);
-When(/^I open "([^"]*)" url$/, function (url) {
+When('I open {string} url', function (url) {
     return browser.get(url);
 });
 
-Then(/^Page title should be "([^"]*)"$/, async function (title) {
+Then('Page title should be {string}', async function (title) {
     const pageTitle = await browser.getTitle();
     expect(pageTitle).to.be.equal(title);
 })
 
-When(/^I wait "([^"]*)" seconds$/, function (sec) {
+When('I wait "{int}" seconds', function (sec) {
     return browser.sleep(sec*1000);
 })
