7a74bd86c8e71329b11dedfff5c4f231cc5fbdad
watsonik <watsonik@gmail.com>
2020-11-11 00:04:04 +0300

    Optional parameters in step definition

--- test/step_definitions/browser_steps.js
+++ test/step_definitions/browser_steps.js
@@ -5,9 +5,13 @@ When('I open {string} url', function (url) {
     return browser.get(url);
 });
 
-Then('Page title should be {string}', async function (title) {
+Then(/^Page title should (not )?be "([^"]*)"$/, async function (notArg, title) {
     const pageTitle = await browser.getTitle();
-    expect(pageTitle).to.be.equal(title);
+    if (notArg) {
+        expect(pageTitle).to.not.be.equal(title);
+    } else {
+        expect(pageTitle).to.be.equal(title);
+    }
 })
 
 When('I wait "{int}" seconds', function (sec) {
